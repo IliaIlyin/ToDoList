9077e5ec1e4ec1ba806910627a64439f81a5fac1
Ilyin-Ilya <fate98765@gmail.com>
2020-07-21 13:11:42 +0300

    completed postpone task realization

--- TaskService.cpp
+++ TaskService.cpp
@@ -11,11 +11,18 @@ void TaskService::postponeTask(TaskEntity task, time_t dueDate) {
                 task.getTask()->getPriority(),task.getTask()->getLabel());
 
         TaskEntity taskEntity(t,task.getTaskId(),task.checkStatus(),task.getSubtasks());
+
         tasks.erase(iter);
         cleanPrioritiesWithCertainPriority(t.getPriority());
         cleanDatesWithCertainDate(t.getDate());
         cleanLabelsWithCertainLabel(t.getLabel());
-        tasks.insert(std::make_shared<TaskEntity>(taskEntity));
+
+        auto ptr=std::make_shared<TaskEntity>(taskEntity);
+
+        tasks.insert(ptr);
+        priorities.insert(std::pair<Task::Priority,std::weak_ptr<TaskEntity> > (task.getTask()->getPriority(),ptr));
+        dates.insert(std::pair<time_t,std::weak_ptr<TaskEntity> > (task.getTask()->getDate(),ptr));
+        labels.insert(std::pair<std::string,std::weak_ptr<TaskEntity> > (task.getTask()->getLabel(),ptr));
     }
     else{
         std::cout<<"task "<<task.getTask()->getName()<<" Not found"<<std::endl;
