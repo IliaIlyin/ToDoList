c0a99b07270323cb27a0a3623fe6556336ef8c8b
Ilyin-Ilya <fate98765@gmail.com>
2020-07-26 20:19:05 +0300

    added some tests

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 #include "TaskService.h"
 int main() {
     TaskService taskService;
-    taskService.addTask("Lol");
+    std::shared_ptr<Task> ptr = std::make_shared<Task>(Task::createTask("LOL"));
+    taskService.addTask(ptr.operator*());
+    Task t=Task::createTask("RRR",18,Task::Priority::SECOND,"epicenter");
+    std::shared_ptr<TaskEntity> taskEntity = std::make_shared<TaskEntity>(t,taskService.getIdGenerator());
+    taskService.insertEntity(taskEntity);
+    taskService.addSubTaskToParent(taskEntity,ptr.operator*());
+    taskService.completeTask(taskEntity);
     taskService.showAllByPriority();
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
