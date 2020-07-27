ceee40a3009ce91c787ce38d068525200655ef9a
Ilyin-Ilya <fate98765@gmail.com>
2020-07-27 08:42:36 +0300

    provided tests for task entity

---
+++ Tests/TaskEntityTest.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Ilya on 7/26/2020.
+//
+
+#include<gtest/gtest.h>
+#include<gmock/gmock.h>
+
+#include "C:\Users\Ilya\CLionProjects\toDoList\TaskEntity.h"
+using testing::Eq;
+
+class TaskEntityTest : public ::testing::Test {
+
+};
+
+TEST_F(TaskEntityTest,shouldCreateTaskEntity){
+    Task task = Task::createTask("Lol",200,Task::Priority::FIRST,"label");
+    IdGenerator idGenerator;
+    std::vector<std::shared_ptr<TaskEntity>> vec;
+    TaskID taskId(0);
+    TaskEntity taskEntity(task,idGenerator);
+    ASSERT_TRUE(taskEntity.getTask()->getName()== task.getName());
+    ASSERT_TRUE(taskEntity.getTask()->getLabel()==task.getLabel());
+    ASSERT_EQ(taskEntity.getTask()->getDate(),task.getDate());
+    ASSERT_EQ(taskEntity.getTask()->getPriority(),task.getPriority());
+    ASSERT_EQ((taskId.getId()),(taskEntity.getTaskId().getId()));
+    ASSERT_EQ(false,taskEntity.checkStatus());
+    ASSERT_EQ(vec,taskEntity.getSubtasks());
+}
---
+++ Tests/TaskServiceTest.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Ilya on 7/26/2020.
+//
+#include<gtest/gtest.h>
+#include<gmock/gmock.h>
+
+#include "C:\Users\Ilya\CLionProjects\toDoList\TaskService.h"
+
+using testing::Eq;
+
+class TaskServiceTest : public ::testing::Test {
+public:
+    std::shared_ptr<Task> ptr;
+    TaskService taskService;
+    Task t = Task::createTask("RRR", 18, Task::Priority::SECOND, "epicenter");
+    std::shared_ptr<TaskEntity> taskEntity;
+
+    TaskServiceTest() {
+        ptr = std::make_shared<Task>(Task::createTask("LOL"));
+        taskEntity = std::make_shared<TaskEntity>(t, taskService.getIdGenerator());
+    }
+};
+
+
