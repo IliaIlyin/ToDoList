dc94459ab53d9c8234e583e2b8de634f02dfb558
Ilyin-Ilya <fate98765@gmail.com>
2020-07-31 10:50:30 +0300

    created convertor from dto to task entity

---
+++ Model/DTOconvertor.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ilya on 7/31/2020.
+//
+
+#include "DTOconvertor.h"
+TaskDTO DTOconvertor::convert(TaskEntity task_entity) {
+  for(auto i=task_entity.getSubtasks().begin();i!=task_entity.getSubtasks().end();i++){
+
+  }
+}
---
+++ Model/DTOconvertor.h
@@ -0,0 +1,15 @@
+//
+// Created by Ilya on 7/31/2020.
+//
+
+#ifndef TODOLIST_MODEL_DTOCONVERTOR_H_
+#define TODOLIST_MODEL_DTOCONVERTOR_H_
+#include"TaskEntity.h"
+#include"TaskDTO.h"
+class DTOconvertor {
+ public:
+  TaskDTO convert(TaskEntity task_entity);
+  TaskEntity convert(TaskDTO task_dto);
+};
+
+#endif //TODOLIST_MODEL_DTOCONVERTOR_H_
