6a95a95278ef3032171a12d7ac8a5ce3f53eb77b
Ilyin-Ilya <fate98765@gmail.com>
2020-08-02 12:11:36 +0300

    added storage class realization

--- Model/Storage.cpp
+++ Model/Storage.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Storage.h"
+bool Storage::addTask(TaskEntity &task_entity) {
+  tasks_.insert(std::make_pair(task_entity.getTaskId(), task_entity));
+  return true;
+}
+bool Storage::deleteTask(TaskEntity &task_entity) {
+  tasks_.erase(task_entity.getTaskId());
+  return true;
+}
+TaskEntity Storage::getTask(TaskEntity &task_entity) {
+  return tasks_.find(task_entity.getTaskId())->second;
+}
+
+bool Storage::addSubTaskToParent(std::shared_ptr<TaskEntity> parent, TaskEntity &task_entity) {
+  parent->addsubtask(std::make_shared<TaskEntity>(task_entity));
+  return true;
+}
--- Model/Storage.h
+++ Model/Storage.h
@@ -4,9 +4,17 @@
 
 #ifndef TODOLIST_MODEL_STORAGE_H_
 #define TODOLIST_MODEL_STORAGE_H_
-
+#include<unordered_map>
+#include"TaskEntity.h"
+#include"HashTaskID.h"
 class Storage {
-
+ public:
+  bool addTask(TaskEntity &task_entity);
+  bool deleteTask(TaskEntity &task_entity);
+  TaskEntity getTask(TaskEntity &task_entity);
+  bool addSubTaskToParent(std::shared_ptr<TaskEntity> parent, TaskEntity &task_entity);
+ private:
+  std::unordered_map<TaskID, TaskEntity, HashTaskID> tasks_;
 };
 
 #endif //TODOLIST_MODEL_STORAGE_H_
