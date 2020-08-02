ef11f12127112b5b8217451eac7d0922d16ab173
Ilyin-Ilya <fate98765@gmail.com>
2020-08-02 23:31:56 +0300

    added interface for views

---
+++ Views/GeneralView.h
@@ -0,0 +1,21 @@
+//
+// Created by Ilya on 8/2/2020.
+//
+
+#ifndef TODOLIST_VIEWS_GENERALVIEW_H_
+#define TODOLIST_VIEWS_GENERALVIEW_H_
+#include"../Model/TaskEntity.h"
+#include<memory>
+#include<vector>
+
+
+template<typename T>
+class GeneralView {
+
+  virtual std::vector<std::weak_ptr<TaskEntity>> showAll()=0;
+  virtual std::vector<std::weak_ptr<TaskEntity>> showToday()=0;
+  virtual std::vector<std::weak_ptr<TaskEntity>> showDueDate(time_t date)=0;
+
+};
+
+#endif //TODOLIST_VIEWS_GENERALVIEW_H_
