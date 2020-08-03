90ce2454669b2fe43df3e75cab4309556a54c63d
Ilyin-Ilya <fate98765@gmail.com>
2020-08-03 22:43:40 +0300

    storage returns shared ptrs

--- Model/StorageService.cpp
+++ Model/StorageService.cpp
@@ -4,7 +4,7 @@
 
 #include "StorageService.h"
 
-bool StorageService::addTask(std::string taskName, time_t date, Task::Priority priority, std::string label) {
+std::shared_ptr<TaskEntity> StorageService::addTask(std::string taskName, time_t date, Task::Priority priority, std::string label) {
   Task task = Task::createTask(std::move(taskName), date, priority, std::move(label));
   TaskEntity taskEntity = TaskEntity::createTaskEntity(task, this->id_generator_);
   return storage_.addTask(taskEntity);
@@ -14,7 +14,7 @@ bool StorageService::deleteTask(TaskEntity &task_entity) {
   return storage_.deleteTask(task_entity);
 }
 
-bool StorageService::addSubTaskToParent(TaskEntity &parent, std::string taskName, time_t date,
+std::shared_ptr<TaskEntity> StorageService::addSubTaskToParent(TaskEntity &parent, std::string taskName, time_t date,
                                         Task::Priority priority,
                                         std::string label) {
   Task t = Task::createTask(std::move(taskName), date, priority, std::move(label));
@@ -22,29 +22,29 @@ bool StorageService::addSubTaskToParent(TaskEntity &parent, std::string taskName
   return this->addSubTaskToParent(parent, taskEntity);
 }
 
-TaskEntity StorageService::getTask(TaskEntity &task_entity) {
+std::shared_ptr<TaskEntity> StorageService::getTask(TaskEntity &task_entity) {
   return storage_.getTask(task_entity);
 }
 
-bool StorageService::addTask(Task &task) {
+std::shared_ptr<TaskEntity> StorageService::addTask(Task &task) {
   TaskEntity taskEntity = TaskEntity::createTaskEntity(task, this->id_generator_);
   return storage_.addTask(taskEntity);
 }
 
-bool StorageService::addSubTaskToParent(TaskEntity &parent, Task &task) {
+std::shared_ptr<TaskEntity> StorageService::addSubTaskToParent(TaskEntity &parent, Task &task) {
   TaskEntity taskEntity = TaskEntity::createTaskEntity(task, this->id_generator_);
   return this->addSubTaskToParent(parent, taskEntity);
 }
 
-bool StorageService::addSubTaskToParent(TaskEntity &parent, TaskEntity &task_entity) {
+std::shared_ptr<TaskEntity> StorageService::addSubTaskToParent(TaskEntity &parent, TaskEntity &task_entity) {
   auto iter = storage_.getTask(parent);
-  iter.addsubtask(std::make_shared<TaskEntity>(task_entity));
-  storage_.addTask(iter);
-  return true;
+  iter.operator*().addsubtask(std::make_shared<TaskEntity>(task_entity));
+  return storage_.addTask(iter.operator*());
 }
 
+
 bool StorageService::postponeTask(TaskEntity &task, time_t dueDate) {
-  auto iter = storage_.getTask(task);
+  auto iter=storage_.getTask(task);
   Task t = Task::createTask(task.getTask().getName(),
                             dueDate + task.getTask().getDate(),
                             task.getTask().getPriority(), task.getTask().getLabel());
@@ -52,12 +52,13 @@ bool StorageService::postponeTask(TaskEntity &task, time_t dueDate) {
   TaskEntity taskEntity(t, task.getTaskId(), task.checkStatus(),
                         task.getSubtasks());
 
-  this->deleteTask(task);
-  return storage_.addTask(taskEntity);
+  this->deleteTask(iter.operator*());
+  storage_.addTask(taskEntity);
+  return true;
 }
 
 bool StorageService::completeTask(TaskEntity &task) {
-  auto it = storage_.getTask(task);
+  auto it = storage_.getTask(task).operator*();
   it.completeTask();
   auto vec = it.getSubtasks();
   for (auto i = vec.begin(); i != vec.end(); i++) {
