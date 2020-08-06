4838a1be81de253d9f0b63de436c50acd5bfa5c3
Ilyin-Ilya <fate98765@gmail.com>
2020-08-06 10:46:51 +0300

    changed dto convertor

--- src/Model/dtoConvertor.cpp
+++ src/Model/dtoConvertor.cpp
@@ -5,19 +5,14 @@
 #include "dtoConvertor.h"
 
 TaskDTO dtoConvertor::convert(TaskEntity &task_entity) {
-  std::vector<std::shared_ptr<TaskDTO> > vec;
-  for (auto i = task_entity.getSubtasks().begin(); i != task_entity.getSubtasks().end(); i++) {
-    vec.push_back(std::make_shared<TaskDTO>(convert(i->operator*())));
-  }
-  return TaskDTO(task_entity.getTask(), task_entity.getTaskId(), task_entity.checkStatus(), vec);
+  return TaskDTO(task_entity.getTask(), task_entity.getTaskId(), task_entity.checkStatus());
 }
 
 TaskEntity dtoConvertor::convert(TaskDTO &task_dto) {
-  std::vector<std::shared_ptr<TaskEntity> > vec;
-  for (auto i = task_dto.getSubtasks().begin(); i != task_dto.getSubtasks().end(); i++) {
-    vec.push_back(std::make_shared<TaskEntity>(convert(i->operator*())));
-  }
-  return TaskEntity(task_dto.getTask(), task_dto.getTaskId(), task_dto.checkStatus(), vec);
+  return TaskEntity(task_dto.getTask(),
+                    task_dto.getTaskId(),
+                    task_dto.checkStatus(),
+                    std::vector<std::shared_ptr<TaskEntity>>());
 }
 
 std::vector<TaskDTO> dtoConvertor::convert(std::vector<std::weak_ptr<TaskEntity>> vec) {
