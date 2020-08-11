4666ad9cf65908fe9675edda5939d82fb8370f81
IliaIlyin <illia.ilin@teamdev.com>
2020-08-11 12:01:47 +0300

    provided tests for task dto

--- Tests/TaskDtoTest.cpp
+++ Tests/TaskDtoTest.cpp
@@ -15,3 +15,24 @@ class TaskDTOTest : public ::testing::Test {
 
 };
 
+TEST_F(TaskDTOTest, shouldCreateDTO) {
+  Task task = Task::createTask("Lol", boost::gregorian::date{2000, 12, 9}, Task::Priority::FIRST, "label");
+  IdGenerator idGenerator;
+  TaskID taskId(0);
+  TaskDTO taskDTO = TaskDTO::createTaskDTO(task, idGenerator);
+  ASSERT_EQ(taskDTO.getTask(), task);
+  ASSERT_EQ(taskDTO.getTaskId().getId(), 0);
+  ASSERT_EQ(taskDTO.getTaskId(), taskId);
+  ASSERT_EQ(taskDTO.checkStatus(), false);
+  TaskDTO dto(task, taskId, true);
+  ASSERT_EQ(dto.getTask(), task);
+  ASSERT_EQ( dto.getTaskId().getId(), 0);
+  ASSERT_EQ(dto.getTaskId(), taskId);
+  ASSERT_EQ(dto.checkStatus(), true);
+  TaskDTO dto2(task, taskId, false);
+  ASSERT_EQ(dto2.getTask(), task);
+  ASSERT_EQ(dto2.getTaskId().getId(), 0);
+  ASSERT_EQ(dto2.getTaskId(), taskId);
+  ASSERT_EQ(dto2.checkStatus(), false);
+}
+
