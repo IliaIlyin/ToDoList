d37c24fb0f9d788f803e63e1e7b3c1e4e6d799ac
IliaIlyin <illia.ilin@teamdev.com>
2020-08-21 17:34:16 +0300

    tests

--- CLI/Validator.cpp
+++ CLI/Validator.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "Validator.h"
-Validator::ValidateResult Validator::validateShowAll(std::string s) {
+Validator::ValidateResult Validator::validateShowAll(const std::string& s) {
   std::string f = "show all";
   if (s == f) {
     return ValidateResult::CORRECT;
@@ -15,7 +15,7 @@ Validator::ValidateResult Validator::validateShowAll(std::string s) {
     return ValidateResult::INCORRECT_COMMAND;
   }
 }
-Validator::ValidateResult Validator::validateShowToday(std::string s) {
+Validator::ValidateResult Validator::validateShowToday(const std::string& s) {
   std::string f = "show today";
   if (s == f) {
     return ValidateResult::CORRECT;
@@ -27,7 +27,7 @@ Validator::ValidateResult Validator::validateShowToday(std::string s) {
     return ValidateResult::INCORRECT_COMMAND;
   }
 }
-Validator::ValidateResult Validator::validateShowLabel(std::string s) {
+Validator::ValidateResult Validator::validateShowLabel(const std::string& s) {
   std::string f = "show label ";
   if (s.substr(0, f.size()) == f) {
     if (s.substr(f.size(), s.size()) != "")
@@ -36,7 +36,7 @@ Validator::ValidateResult Validator::validateShowLabel(std::string s) {
   }
   return ValidateResult::INCORRECT_COMMAND;
 }
-Validator::ValidateResult Validator::validateShowDueDate(std::string s) {
+Validator::ValidateResult Validator::validateShowDueDate(const std::string& s) {
   std::string f = "show due date ";
   if (s.substr(0, f.size()) == f) {
     try {
@@ -49,7 +49,7 @@ Validator::ValidateResult Validator::validateShowDueDate(std::string s) {
   }
   return ValidateResult::INCORRECT_COMMAND;
 }
-Validator::ValidateResult Validator::validateAddTask(std::string s) {
+Validator::ValidateResult Validator::validateAddTask(const std::string& s) {
   std::vector<std::string> results;
   boost::split(results, s, [](char c) { return c == ' '; });
   int k = results.size();
@@ -64,4 +64,29 @@ Validator::ValidateResult Validator::validateAddTask(std::string s) {
   }
 
   return ValidateResult::INCORRECT_COMMAND;
+}
+
+Validator::ValidateResult Validator::validateName(const std::string& s) {
+  if(s!="")
+    return ValidateResult::CORRECT;
+  return ValidateResult::INCORRECT_INPUT_NAME;
+}
+Validator::ValidateResult Validator::validateDate(const std::string& s) {
+  try {
+    boost::gregorian::from_string(s);
+    return ValidateResult::CORRECT;
+  }
+  catch (...) {
+    return ValidateResult::INCORRECT_INPUT_DATE;
+  }
+}
+Validator::ValidateResult Validator::validatePriority(const std::string &s) {
+  if(s=="NONE" || s=="FIRST" || s=="SECOND" || s=="THIRD")
+    return ValidateResult::CORRECT;
+  return ValidateResult::INCORRECT_INPUT_PRIORITY;
+}
+Validator::ValidateResult Validator::validateLabel(const std::string &s) {
+  if(s!="")
+    return ValidateResult::CORRECT;
+  return ValidateResult::INCORRECT_INPUT_LABEL;
 }
--- CLI/Validator.h
+++ CLI/Validator.h
@@ -11,19 +11,30 @@ class Validator {
  enum class ValidateResult{
    CORRECT,
    INCORRECT_COMMAND,
-   INCORRECT_INPUT,
+   INCORRECT_INPUT_NAME,
+   INCORRECT_INPUT_DATE,
+   INCORRECT_INPUT_PRIORITY,
+   INCORRECT_INPUT_LABEL,
  };
- static ValidateResult validateShowAll(std::string s);
- static ValidateResult validateShowToday(std::string s);
- static ValidateResult validateShowLabel(std::string s);
- static ValidateResult validateShowDueDate(std::string s);
- static ValidateResult validateAddTask(std::string s);
- static ValidateResult validateAddSubTask(std::string s);
- static ValidateResult validateGetTask(std::string s);
- static ValidateResult validateGetSubTasks(std::string s);
- static ValidateResult validatePostponeTask(std::string s);
- static ValidateResult validateCompleteTask(std::string s);
- static ValidateResult validateDeleteTask(std::string s);
+
+ public:
+  static ValidateResult validateName(const std::string& s);
+  static ValidateResult validateDate(const std::string& s);
+  static ValidateResult validatePriority(const std::string& s);
+  static ValidateResult validateLabel(const std::string& s);
+
+ public:
+ static ValidateResult validateShowAll(const std::string& s);
+ static ValidateResult validateShowToday(const std::string& s);
+ static ValidateResult validateShowLabel(const std::string& s);
+ static ValidateResult validateShowDueDate(const std::string& s);
+ static ValidateResult validateAddTask(const std::string& s);
+ static ValidateResult validateAddSubTask(const std::string& s);
+ static ValidateResult validateGetTask(const std::string& s);
+ static ValidateResult validateGetSubTasks(const std::string& s);
+ static ValidateResult validatePostponeTask(const std::string& s);
+ static ValidateResult validateCompleteTask(const std::string& s);
+ static ValidateResult validateDeleteTask(const std::string& s);
 
 };
 
